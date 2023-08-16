a04fcc737107159e7a0b26882c4cc829c82e70f3
maaaashi <doyaseizin.7@gmail.com>
2023-08-16 20:09:56 +0900

    一旦動くところまで

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,68 @@
+'use client'
+
 import ComplexPlaneChart from '@/components/ComplexChart'
+import { FormEvent, useState } from 'react'
+
+export type Point = {
+  x: number
+  y: number
+  rad: number
+}
 
 export default function Home() {
-  const data = [
-    { x: -1, y: 0 },
-    { x: 1, y: 0 },
-    { x: 0, y: 1.732 },
-    { x: -1, y: 0 },
-  ]
+  const [userInput, setUserInput] = useState(3)
+
+  const A: Point = {
+    x: Math.cos(Math.PI / 2),
+    y: Math.sin(Math.PI / 2),
+    rad: Math.PI / 2,
+  }
+  const B: Point = {
+    x: Math.cos((Math.PI * 7) / 6),
+    y: Math.sin((Math.PI * 7) / 6),
+    rad: (Math.PI * 7) / 6,
+  }
+  const C: Point = {
+    x: Math.cos((Math.PI * 11) / 6),
+    y: Math.sin((Math.PI * 11) / 6),
+    rad: (Math.PI * 11) / 6,
+  }
+  const [data, setData] = useState<Point[]>([A, B, C, A])
+
+  const submitHandler = (e: FormEvent) => {
+    e.preventDefault()
+    let changeData: Point[] = [A]
+    do {
+      const last = changeData[changeData.length - 1]
+      const rad = last.rad + (2 * Math.PI) / userInput
+      const point: Point = {
+        x: Math.cos(rad),
+        y: Math.sin(rad),
+        rad,
+      }
+      changeData.push(point)
+    } while (Math.sin(changeData[changeData.length - 1].rad) !== 1)
+
+    setData(changeData)
+  }
 
   return (
     <main className='h-screen'>
       <ComplexPlaneChart data={data} />
+      <form onSubmit={submitHandler}>
+        <input
+          type='text'
+          name='number'
+          value={userInput}
+          className='input w-full max-w-xs'
+          onChange={(e) => {
+            setUserInput(+e.target.value)
+          }}
+        />
+        <button className='btn btn-primary' type='submit'>
+          作成
+        </button>
+      </form>
     </main>
   )
 }
--- src/components/ComplexChart.tsx
+++ src/components/ComplexChart.tsx
@@ -9,14 +9,20 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js'
+import { Point } from '@/app/page'
+import { FC } from 'react'
 
 ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend)
 
-const ComplexChart = ({ data }) => {
+interface Props {
+  data: Point[]
+}
+
+const ComplexChart: FC<Props> = ({ data }) => {
   const chartData = {
     datasets: [
       {
-        data: data,
+        data,
         backgroundColor: 'rgba(75,192,192,0.4)',
         borderColor: 'rgba(75,192,192,1)',
         borderWidth: 1,
