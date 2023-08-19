5df21b55cbfea9bfe32a2b2932524b3b7e0738f2
maaaashi <doyaseizin.7@gmail.com>
2023-08-19 19:42:15 +0900

    n次関数

--- src/app/n-order-function/page.tsx
+++ src/app/n-order-function/page.tsx
@@ -9,20 +9,26 @@ export type Point = {
   rad: number
 }
 
+type Range = {
+  min: number
+  max: number
+}
+
 const Home: FC = () => {
-  // const [userInput, setUserInput] = useState('3')
+  const [userInput, setUserInput] = useState('2')
 
   const [xValues, setXValue] = useState<number[]>([])
+  const [xRange, setXRange] = useState<Range>({ min: -10, max: 10 })
+  const [yRange, setYRange] = useState<Range>({ min: 0, max: 100 })
   const [yValues, setYValue] = useState<number[]>([])
 
-  const myEquation = (x: number): number => Math.pow(x, 2)
-
   useEffect(() => {
+    const equation = (x: number): number => Math.pow(x, 2)
+
     for (let x = -10; x <= 10; x += 0.1) {
-      const y = myEquation(x)
-      console.log(y)
-      setXValue((c) => [...c, xValues.push(x)])
-      setYValue((c) => [...c, yValues.push(y)])
+      const y = equation(x)
+      setXValue((c) => [...c, x])
+      setYValue((c) => [...c, y])
     }
   }, [])
 
@@ -35,7 +41,7 @@ const Home: FC = () => {
         borderColor: 'rgba(75,192,192,1)',
         borderWidth: 1,
         showLine: true,
-        pointRadius: 5,
+        pointRadius: 0,
       },
     ],
   }
@@ -45,13 +51,13 @@ const Home: FC = () => {
     scales: {
       x: {
         display: true,
-        max: 10,
-        min: -10,
+        max: xRange.max,
+        min: xRange.min,
       },
       y: {
         display: true,
-        max: 100,
-        min: 0,
+        max: yRange.max,
+        min: yRange.min,
       },
     },
     plugins: {
@@ -59,7 +65,7 @@ const Home: FC = () => {
         display: false,
       },
       tooltip: {
-        enabled: true,
+        enabled: false,
       },
     },
     responsive: true,
@@ -67,22 +73,48 @@ const Home: FC = () => {
 
   const submitHandler = (e: FormEvent) => {
     e.preventDefault()
-    // if (!+userInput || +userInput < 2) return
+    if (!+userInput) return
+    setXValue([])
+    setYValue([])
+    setXRange({
+      min: 0,
+      max: 0,
+    })
+    setYRange({
+      min: 0,
+      max: 0,
+    })
+
+    const equation = (x: number): number => Math.pow(x, +userInput)
+
+    for (let x = -10; x <= 10; x += 0.1) {
+      const y = equation(x)
+      setXValue((c) => [...c, x])
+      setXRange((c) => {
+        return {
+          max: c.max > x ? c.max : x,
+          min: c.min > x ? x : c.min,
+        }
+      })
+      setYRange((c) => {
+        return {
+          max: c.max > y ? c.max : y,
+          min: c.min > y ? y : c.min,
+        }
+      })
+      setYValue((c) => [...c, y])
+    }
   }
 
   return (
     <main className='flex flex-col items-center p-5 h-full'>
-      <h2>正n/m角形</h2>
-      {/* {xValues.map((v, i) => {
-        return <div key={i}>{v}</div>
-      })}
-      {yValues.map((v, i) => {
-        return <div key={i}>{v}</div>
-      })} */}
+      <h2>n次関数</h2>
+      <p>
+        y = x<sup>n</sup>
+      </p>
       <ComplexPlaneChart data={chartData} options={options} />
-      {/* <form onSubmit={submitHandler} className='flex items-center gap-5'>
+      <form onSubmit={submitHandler} className='flex items-center gap-5'>
         <div>
-          <span>正</span>
           <input
             type='text'
             value={userInput}
@@ -91,12 +123,12 @@ const Home: FC = () => {
               setUserInput(e.target.value)
             }}
           />
-          <span>角形</span>
+          <span>次関数</span>
         </div>
         <button className='btn btn-primary' type='submit'>
           作成
         </button>
-      </form> */}
+      </form>
     </main>
   )
 }
